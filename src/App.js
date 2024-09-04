f80631b20f6ae30f20b72e820ab1f4e427a26138
Vyshnav <vyshnavk78@gmail.com>
2024-09-04 23:38:48 +0530

    useState on item

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [newItem, setNewitem] = useState("");
+  /* useState intakes a default value as inp and here since the new item 
+    (the entry someone adds, when webpage starts, its empty)
+  */
+
   return (
     <>
       <form className='new-item-form'>
         <div className='form-row'>
           <label htmlFor='item'>New item</label>
-          <input type='text' id='item'></input>
+          <input 
+            value={newItem}
+            onChange={event => {setNewitem(event.target.value)
+
+            console.log(newItem);}
+            } type='text' id='item'></input>
+            
         </div>
         <button className='btn'>Add</button>
       </form>
