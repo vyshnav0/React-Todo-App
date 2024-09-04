6fca8f358a59234a3412bf5303c3c27ae988c081
Vyshnav <vyshnavk78@gmail.com>
2024-09-05 00:57:54 +0530

    unique key and input clr on each add

--- src/App.js
+++ src/App.js
@@ -7,19 +7,20 @@ function App() {
   /* useState intakes a default value as inp and here since the new item 
     (the entry someone adds, when webpage starts, its empty)
   */
- const [todos, setTodos] = useState([]);
+  const [todos, setTodos] = useState([]);
 
   function handleSubmit(e) {
     e.preventDefault();
     // prevents reloading each time you submit the form
 
-    setTodos((currentTodos)=>{
+    setTodos((currentTodos) => {
       return [...currentTodos, {
         id: crypto.randomUUID(),
         title: newItem,
         completed: false
       }]
     });
+    setNewitem("");
   }
   console.log(todos);
 
@@ -28,22 +29,24 @@ function App() {
       <form onSubmit={handleSubmit} className='new-item-form'>
         <div className='form-row'>
           <label htmlFor='item'>New item</label>
-          <input 
+          <input
             value={newItem}
             onChange={event => setNewitem(event.target.value)}
             type='text' id='item'>
           </input>
-            
+
         </div>
         <button className='btn'>Add</button>
       </form>
       <h1 className='header'>ToDo List</h1>
       <ul className='list'>
-        {todos.map(singleTodo=>{
-          return <li>
-              <label><input type='checkbox' checked={singleTodo.completed}/>{singleTodo.title}</label>
+        {todos.map(singleTodo => {
+          return (
+            <li key={singleTodo.id}>
+              <label><input type='checkbox' checked={singleTodo.completed} />{singleTodo.title}</label>
               <button className='btn btn-danger'>Delete</button>
             </li>
+          )
         })}
       </ul>
     </>
