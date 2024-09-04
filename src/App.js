3f0f24cf735708631fe3833301b225bde1527bf5
Vyshnav <vyshnavk78@gmail.com>
2024-09-04 23:18:18 +0530

    rough design

--- src/App.js
+++ src/App.js
@@ -3,7 +3,24 @@ import './App.css';
 function App() {
   return (
     <>
-    hi
+      <form className='new-item-form'>
+        <div className='form-row'>
+          <label htmlFor='item'>New item</label>
+          <input type='text' id='item'></input>
+        </div>
+        <button className='btn'>Add</button>
+      </form>
+      <h1 className='header'>ToDo List</h1>
+      <ul className='list'>
+        <li>
+          <label><input type='checkbox' />Item1</label>
+          <button className='btn btn-danger'>Delete</button>
+        </li>
+        <li>
+          <label><input type='checkbox' />Item2</label>
+          <button className='btn btn-danger'>Delete</button>
+        </li>
+      </ul>
     </>
   );
 }
