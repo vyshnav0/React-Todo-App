e6c5d7830036b2850ac093ce7a48cc2f3cc1dcf1
Vyshnav <vyshnavk78@gmail.com>
2024-09-05 00:40:30 +0530

    new todo are added to todos array

--- src/App.js
+++ src/App.js
@@ -7,23 +7,38 @@ function App() {
   /* useState intakes a default value as inp and here since the new item 
     (the entry someone adds, when webpage starts, its empty)
   */
+ const [todos, setTodos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // prevents reloading each time you submit the form
+
+    setTodos((currentTodos)=>{
+      return [...currentTodos, {
+        id: crypto.randomUUID(),
+        title: newItem,
+        completed: false
+      }]
+    });
+  }
+  console.log(todos);
 
   return (
     <>
-      <form className='new-item-form'>
+      <form onSubmit={handleSubmit} className='new-item-form'>
         <div className='form-row'>
           <label htmlFor='item'>New item</label>
           <input 
             value={newItem}
-            onChange={event => {setNewitem(event.target.value)
-
-            console.log(newItem);}
-            } type='text' id='item'></input>
+            onChange={event => setNewitem(event.target.value)}
+            type='text' id='item'>
+          </input>
             
         </div>
         <button className='btn'>Add</button>
       </form>
       <h1 className='header'>ToDo List</h1>
+      
       <ul className='list'>
         <li>
           <label><input type='checkbox' />Item1</label>
